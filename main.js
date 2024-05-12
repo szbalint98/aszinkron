b199606c5b4b060505ba0a9dc6f831eee6295cf6
baalintka <ballint3@gmail.com>
2024-05-12 20:21:57 +0200

    kiirja a bodyba

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 
 console.log("1. üzi")
-export function getKeres(vegpont){
+export function getKeres(vegpont, callback){
     fetch(vegpont)
         .then((response) => response.json())
-        .then((data) => {console.log(data)})
-        .catch((err) => {console.log(err)})
+        .then((data) => { callback(data); })
+        .catch((err) => { console.log(err); });
 }
-console.log("2. üzi")
+
 getKeres("adatok.json",megjel)
 getKeres("https://pokeapi.co/api/v2/pokemon/ditto",megjelpokemon)
-
+console.log("2. üzi")
 function megjel(adat){
     console.log(adat)
-    console.log();
+    
 }
 function megjel2(adat){
     $("body").append(`<h2>${adat.activity}</h2>`)
